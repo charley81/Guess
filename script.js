7ef9cb7c50fd8a12616233e6d23dae54aac4c046
ChrisKnowsDev <chrisharley81@gmail.com>
2022-07-04 12:14:05 -0400

    functionality for guess submit complete

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// place cursor in guess input
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.guess').focus()
+})
+
+let randomNumber = Math.floor(Math.random() * 20) + 1
+let highscore = 0
+let score = 20
+
+// create message function
+const message = text => (document.querySelector('.message').textContent = text)
+
+const submit = document
+  .querySelector('.submit')
+  .addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value)
+
+    // if no number is entered
+    if (!guess) {
+      message('⛔️ You must enter a number')
+    }
+
+    // if guess does not equal random number
+    else if (guess !== randomNumber) {
+      if (score > 5) {
+        guess > randomNumber ? message('👆 Too high') : message('👇 Too low')
+        score -= 5
+        document.querySelector('.current-score').textContent = score
+        document.querySelector('.guess').value = ''
+        document.querySelector('.guess').focus()
+      } else {
+        document.querySelector('.guess').disabled = true
+        message('🥲 GAME OVER')
+        document.querySelector('.current-score').textContent = 0
+      }
+    }
+
+    // user won
+    else {
+      message('🎉 Congrats, you won')
+      document.querySelector('.guess').disabled = true
+      document.querySelector('.number').textContent = randomNumber
+      document.querySelector('body').style.backgroundColor = '#066163'
+    }
+  })
